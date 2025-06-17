7bac74d3b1c4e6a894b735cfe9bc232bf7cdf785
Miguel <migueljm1008@gmail.com>
2025-06-17 17:32:53 +0200

    Un poco de logica

---
+++ script.js
@@ -0,0 +1,27 @@
+//Multiplos 3
+let multiplos3 = [];
+for(let a= 3; a<= 1000; a+= 3){
+    multiplos3.push([a])
+}
+
+//Multiplos 5
+let multiplos5 = [];
+for(let b= 5; b<= 1000; b+= 5){
+    multiplos5.push([b])
+}
+//multiplos 3 y 5
+//let multiplos35 = [];
+
+//Contador
+let contador = () => {
+    for(let i= 0; i<= 1000; i++){
+        console.log(i)
+        if(multiplos3.includes(i)){
+            console.log('Fizz');
+        } else if(multiplos5.includes(i)){
+            console.log('Buzz')
+        }else if(multiplos3.includes(i) && multiplos5.includes(i)){
+            console.log()
+        }
+    }
+}
